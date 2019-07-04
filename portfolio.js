bf78e9fd8e960a01811a7b283e227a504688ad86
Vignesh Selvaraj <30921385+vignesh77080@users.noreply.github.com>
2019-07-04 15:31:56 +0530

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -11,29 +11,24 @@ var footerwrap = document.querySelector('.footer-wrap');
 
 // onclick for the generating the id of header
 
-function myfunction(id){
-    console.log(id);  
-    var class_name = document.getElementsByClassName(''+id)[0].getAttribute('class');
-    console.log(class_name)
-    if(class_name.search(id) >= 0){
-        console.log('yes');
-        var all_active = document.querySelectorAll('.active');
-        var blank = document.querySelector('.empty-div');
-        blank.classList.add('blank-page');
-        for(let i=0 ; i < all_active.length ; i++){
-            all_active[i].classList.remove('active');
-        }
-        setInterval(function(){
-        var add_active = document.querySelector('.'+id);
-        add_active.classList.add('active');
-        },2000)
-        
-        setTimeout(function(){
-            blank.classList.remove('blank-page');
-        },5000)
-    }
-    else{
-        console.log('no');
-    }
+function myfunction(class_of){
+    var found_class = class_of.className;
+    console.log(found_class);
+    document.body.scrollTop = 0;
+    var blank = document.querySelector('.empty-div');
+    blank.classList.add('blank-page');
+    var find_active = document.querySelectorAll('.active');
+    var break_all = setInterval(function(){
+        for(let i=0 ; i < find_active.length ; i++){
+            find_active[i].classList.remove('active');
+        } 
+        var compare_id = document.querySelector('#'+found_class);
+        compare_id.classList.add('active');
+    }, 500);
+
+    setTimeout(function(){
+        clearInterval(break_all);
+        blank.classList.remove('blank-page');
+    },1000);
 
 }
